3e00ad7d6b27c762813880669f31cd0a6e211d4b
Tom Bechar <tom@run.ai>
2025-02-06 22:21:56 +0200

    fixed base

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  base: '/finanCulator/',
   css: {
     preprocessorOptions: {
       scss: {
